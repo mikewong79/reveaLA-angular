d63af2ffc605b9383746ae94e4f0a5d416d5dfff
Mike Wong <mikewong79@gmail.com>
2014-06-20 17:20:55 -0700

    Nav bar added, not functional, Modal on load

--- app/js/app.js
+++ app/js/app.js
@@ -7,10 +7,11 @@ angular.module('reveaLA', [
   'reveaLA.filters',
   'reveaLA.services',
   'reveaLA.directives',
-  'reveaLA.controllers'
+  'reveaLA.controllers',
+  'mm.foundation'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'ModalDemoCtrl'});
+  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'ModalDemoCtrl'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
